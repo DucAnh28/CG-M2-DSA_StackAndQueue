0cd6790232825324f49445c1375bfea8df7c0f5a
DucAnh28 <ducanhnguyeny2k@gmail.com>
2022-07-25 09:25:18 +0700

    optional Palindrome .

--- Op-BT4-Palindrome/src/Palindrome.java
+++ Op-BT4-Palindrome/src/Palindrome.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class Palindrome {
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter String");
+        String string = scanner.nextLine();
+        Queue<Character> test = new LinkedList<>();
+        for (int i = string.length()-1; i >= 0; i--) {
+            test.add(string.charAt(i));
+        }
+        String reverse = "";
+        while (!test.isEmpty()) {
+            reverse += test.remove();
+        }
+        if (string.equals(reverse)) {
+            System.out.println("This is palindrome ");
+        } else {
+            System.out.println("This is not palindrome ");
+        }
+    }
 }
